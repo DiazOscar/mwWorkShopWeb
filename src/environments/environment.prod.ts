8a63c6758bf7f8117b5d6da6fa0f678741cac109
Oscar Diaz <oscardiaz1201@gmail.com>
2019-06-07 19:10:07 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyARXF2L6OHCMADty-uqFSZZhMBGR0x-VYA",
+    authDomain: "mw-whorkshop.firebaseapp.com",
+    databaseURL: "https://mw-whorkshop.firebaseio.com",
+    projectId: "mw-whorkshop",
+    storageBucket: "mw-whorkshop.appspot.com",
+    messagingSenderId: "1066194706111"
+  }
 };
