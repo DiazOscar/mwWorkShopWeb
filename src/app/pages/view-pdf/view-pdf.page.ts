aedafdb0faffa5e02cb3f3bb7c0f1163d442b1ea
Oscar Diaz <oscardiaz1201@gmail.com>
2019-06-10 19:22:23 +0200

    PDF - Creation

--- src/app/pages/view-pdf/view-pdf.page.ts
+++ src/app/pages/view-pdf/view-pdf.page.ts
@@ -22,12 +22,12 @@ export class ViewPdfPage implements OnInit {
 
   async exportPdf() {
 const div = document.getElementById("pdf");
-    const options = { background: "white", height: 595, width: 845 };
+    const options = { background: "white", height: 845, width: 595 };
     domtoimage.toPng(div, options).then((dataUrl)=> {
         //Initialize JSPDF
         var doc = new jsPDF("p","mm","a4");
         //Add image Url to PDF
-        doc.addImage(dataUrl, 'PNG', 0, 0, 210, 180);
+        doc.addImage(dataUrl, 'PNG', 0, 0, 210, 360);
 
         doc.save("prueba.pdf");
 
